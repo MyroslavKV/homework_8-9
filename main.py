109dd178834da41a4648e789ec22fbaf7f031991
Myroslav Kolesnykov <miroslavkolesnykov@gmail.com>
2025-01-02 18:56:05 +0200

    -

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.responses import RedirectResponse
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
@@ -14,11 +14,27 @@ from models import MovieDB
 init_db()
 
 app = FastAPI()
-
 security = HTTPBasic()
 
+users_db = {
+    "admin": "admin",
+    "user": "password"
+}
+
+def verify_credentials(credentials: HTTPBasicCredentials | None = Depends(security)):
+    print(credentials)
+    correct_password = secrets.compare_digest(credentials.password, users_db.get(credentials.username, ""))
+
+    if not correct_password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect credentials",
+            headers={"Authenticate": "Basic"},
+        )
+    return credentials.username
+
 class Movie(BaseModel):
-    id
+    id: int
     title: str
     director: str
     release_year: int
@@ -39,11 +55,11 @@ def read_root():
     return RedirectResponse(url="/docs")
 
 @app.get("/movies", response_model=list[Movie])
-def get_movies(db: Session = Depends(get_db)):
+def get_movies(username: Annotated[str, Depends(verify_credentials)], db: Session = Depends(get_db)):
     return db.query(MovieDB).all()
 
 @app.post("/movies", response_model=Movie)
-def create_movie(movie: Movie, db: Session = Depends(get_db)):
+def create_movie(movie: Movie,username: Annotated[str, Depends(verify_credentials)], db: Session = Depends(get_db)):
     db_movie = MovieDB(**movie.dict())
     db.add(db_movie)
     db.commit()
@@ -51,14 +67,14 @@ def create_movie(movie: Movie, db: Session = Depends(get_db)):
     return db_movie
 
 @app.get("/movies/{id}", response_model=Movie)
-def get_movie(id: int, db: Session = Depends(get_db)):
+def get_movie(id: int,username: Annotated[str, Depends(verify_credentials)], db: Session = Depends(get_db)):
     movie = db.query(MovieDB).filter(MovieDB.id == id).first()
     if not movie:
         raise HTTPException(status_code=404, detail="Movie not found")
     return movie
 
 @app.delete("/movies/{id}", response_model=Movie)
-def delete_movie(id: int, db: Session = Depends(get_db)):
+def delete_movie(id: int, username: Annotated[str, Depends(verify_credentials)], db: Session = Depends(get_db)):
     movie = db.query(MovieDB).filter(MovieDB.id == id).first()
     if not movie:
         raise HTTPException(status_code=404, detail="Movie not found")
